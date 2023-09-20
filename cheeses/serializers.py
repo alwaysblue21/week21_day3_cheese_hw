f5b47ae38f07c8727de574f44bd4d84d4deaa527
Je Yun <alwaysblue21@gmail.com>
2023-09-19 21:22:14 -0400

    week21_day3_hw

---
+++ cheeses/serializers.py
@@ -0,0 +1,12 @@
+from .models import Cheese
+from rest_framework import serializers
+
+## Class CheeseSerializer is a subclass of serializers.HyperlinkedModelSerializer.
+## For serializing and deserializing data into representations such as json.
+class CheeseSerializer(serializers.HyperlinkedModelSerializer):
+    ## Meta class is for configuring the CheeseSerializer class.
+    class Meta:
+        # model to serialize
+        model = Cheese
+        # fields to show in json
+        fields = ('id', 'name', 'origin_country', 'type')
